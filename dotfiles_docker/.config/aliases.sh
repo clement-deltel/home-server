40974efdd395e292b1288d770e0c64232663505d
Clement Deltel <38040315+clement-deltel@users.noreply.github.com>
2025-02-03 11:17:18 -0600

    ♻️ refactor: split bashrc into multiple files

---
+++ dotfiles_docker/.config/aliases.sh
@@ -0,0 +1,239 @@
+#!/bin/bash
+
+#==============================================================================#
+#               ------- Common --------                                        #
+#==============================================================================#
+alias c='clear'
+alias cls='clear'
+alias ki='kill'
+alias q='exit'
+alias t='time'
+
+# cd
+alias home='cd ~'
+alias ..='cd ..'
+alias ...='cd ..; cd ..'
+alias ....='cd ..; cd ..; cd ..'
+
+# cp
+alias cp='cp -i'
+
+# du
+alias du-sort='du -sh * | sort -h'
+
+# env
+alias env='clear && env | sort'
+
+# grep
+alias grep='grep --color=auto'
+
+# history
+alias h='clear && history | tail -50'
+alias muc='history | awk "{print $2}" | sort | uniq -c | sort -nr | head -10'
+
+# lsof - list open ports
+alias lsop='lsof -i -n -P | grep LISTEN'
+
+# mv - prompt before overwrite
+alias mv='mv -i'
+
+# rm - prompt before every removal
+alias rm='rm -i'
+
+# tree - nice alternative to 'ls'
+alias tree='tree -Csu'
+
+# vim
+alias vimo='vim -o'
+
+#==============================================================================#
+#               ------- ls --------                                            #
+#==============================================================================#
+
+# colorls
+if [ -x "$(command -v colorls)" ]; then
+    alias ls='colorls --color=auto'
+else
+    alias ls='ls --color=auto'
+fi
+
+# ls - list
+alias ll='ls -l'
+# ls - do not ignore entries starting with '.', do not list '.' and '..'
+alias la='ls -Al'
+# ls - list
+alias lah='ls -Ahl'
+# ls - recursive ls
+alias lr='ls -lR'
+# ls - sort by date
+alias lrt='ls -ltr'
+# ls - sort by size
+alias lk='ls -lSr'
+# ls - sort by extension
+alias lx='ls -lXB'
+
+#==============================================================================#
+#               ------- Typos --------                                         #
+#==============================================================================#
+alias :q='exit'
+
+#==============================================================================#
+#               ------- Users --------                                         #
+#==============================================================================#
+alias root='sudo su -'
+alias doc='sudo su - docker'
+
+#==============================================================================#
+#               ------- Server - Directories --------                          #
+#==============================================================================#
+alias {cd,d,svr}home='cd ${SERVER_HOME}'
+alias svr='cd ${SERVER_HOME}'
+
+alias cdsvc='cd ${SERVER_HOME}/services'
+alias svc='cd ${SERVER_HOME}/services'
+
+alias scr='cd ${SERVER_HOME}/scripts'
+alias loghome='cd {SERVER_HOME}/logs'
+
+#==============================================================================#
+#               ------- Server - Environment --------                          #
+#==============================================================================#
+alias set-env=set-env-fn
+alias unset-env=unset-env-fn
+
+#==============================================================================#
+#               ------- Server - Services --------                             #
+#==============================================================================#
+alias {cd,svc}oll='cd ${SERVER_HOME}/services/ai/ollama'
+alias {cd,svc}owu='cd ${SERVER_HOME}/services/ai/open-webui'
+alias {cd,svc}hom='cd ${SERVER_HOME}/services/dashboard/homarr'
+alias {cd,svc}pih='cd ${SERVER_HOME}/services/dns/pihole'
+alias {cd,svc}unb='cd ${SERVER_HOME}/services/dns/unbound'
+alias {cd,svc}act='cd ${SERVER_HOME}/services/finances/actual'
+alias {cd,svc}gro='cd ${SERVER_HOME}/services/inventory/grocy'
+alias {cd,svc}hor='cd ${SERVER_HOME}/services/inventory/hortusfox'
+alias {cd,svc}byt='cd ${SERVER_HOME}/services/management/code/bytestash'
+alias {cd,svc}itt='cd ${SERVER_HOME}/services/management/code/it-tools'
+alias {cd,svc}vau='cd ${SERVER_HOME}/services/management/passwords/vaultwarden'
+alias {cd,svc}gok='cd ${SERVER_HOME}/services/media/sharing/gokapi'
+alias {cd,svc}kav='cd ${SERVER_HOME}/services/media/storage/books/kavita'
+alias {cd,svc}lib='cd ${SERVER_HOME}/services/media/storage/books/librum'
+alias {cd,svc}nex='cd ${SERVER_HOME}/services/media/storage/documents/nextcloud'
+alias {cd,svc}pap='cd ${SERVER_HOME}/services/media/storage/documents/paperless'
+alias {cd,svc}nav='cd ${SERVER_HOME}/services/media/storage/music/navidrome'
+alias {cd,svc}imm='cd ${SERVER_HOME}/services/media/storage/pictures/immich'
+alias {cd,svc}phot='cd ${SERVER_HOME}/services/media/storage/pictures/photoprism'
+alias {cd,svc}jel='cd ${SERVER_HOME}/services/media/storage/videos/jellyfin'
+alias {cd,svc}det='cd ${SERVER_HOME}/services/monitoring/detection'
+alias {cd,svc}jet='cd ${SERVER_HOME}/services/monitoring/jetlog'
+alias {cd,svc}ntf='cd ${SERVER_HOME}/services/monitoring/ntfy'
+alias {cd,svc}pro='cd ${SERVER_HOME}/services/monitoring/prometheus'
+alias {cd,svc}doc='cd ${SERVER_HOME}/services/pdf/docuseal'
+alias {cd,svc}sti='cd ${SERVER_HOME}/services/pdf/stirling-pdf'
+alias {cd,svc}aff='cd ${SERVER_HOME}/services/pkms/affine'
+alias {cd,svc}siy='cd ${SERVER_HOME}/services/pkms/siyuan'
+alias {cd,svc}gua='cd ${SERVER_HOME}/services/remote/guacamole'
+alias {cd,svc}rus='cd ${SERVER_HOME}/services/remote/rustdesk'
+alias {cd,svc}wge='cd ${SERVER_HOME}/services/remote/wg-easy'
+alias {cd,svc}wir='cd ${SERVER_HOME}/services/remote/wireguard'
+alias {cd,svc}enc='cd ${SERVER_HOME}/services/security/enclosed'
+alias {cd,svc}waz='cd ${SERVER_HOME}/services/security/wazuh'
+
+alias {cd,svc}dir='cd ${SERVER_HOME}/services/directus'
+alias {cd,svc}gra='cd ${SERVER_HOME}/services/grafana'
+alias {cd,svc}kop='cd ${SERVER_HOME}/services/kopia'
+alias {cd,svc}lim='cd ${SERVER_HOME}/services/limesurvey'
+alias {cd,svc}min='cd ${SERVER_HOME}/services/minecraft'
+alias {cd,svc}phon='cd ${SERVER_HOME}/services/phoneinfoga'
+alias {cd,svc}tra='cd ${SERVER_HOME}/services/traefik'
+alias {cd,svc}wis='cd ${SERVER_HOME}/services/wishlist'
+
+#==============================================================================#
+#               ------- Server - Scripts --------                              #
+#==============================================================================#
+alias scrall='cd ${SERVER_HOME}/scripts/all'
+alias scrbck='cd ${SERVER_HOME}/scripts/backup'
+alias scrres='cd ${SERVER_HOME}/scripts/restore'
+
+#==============================================================================#
+#               ------- Ansible --------                                       #
+#==============================================================================#
+alias af='ansible localhost -m ansible.builtin.setup'
+
+#==============================================================================#
+#               ------- Bitwarden CLI ------                                   #
+#==============================================================================#
+alias bwc='bw lock && bw logout'
+alias bwl='bw lock'
+alias bwli=bitwarden-login-fn
+alias bwlo='bw logout'
+alias bwo=bitwarden-open-fn
+alias bws=bitwarden-create-session-fn
+alias bwst='bw status | jq'
+alias bwsy='bw sync'
+
+#==============================================================================#
+#               ------- Docker ------                                          #
+#==============================================================================#
+alias d=docker-fn
+alias dcrme=docker-rm-exited-fn
+alias dex=docker-exec-fn
+alias di=docker-inspect-fn
+alias dim='docker images | (sed -u 1q; sort)'
+alias dip=docker-ip-fn
+alias dirmd=docker-image-rm-dangling-fn
+alias dl=docker-logs-fn
+alias dnames=docker-names-fn
+alias dnrm=docker-network-rm-fn
+alias dpsf='docker ps -a --format "table {{ "{{" }}.ID{{ "}}" }}\t{{ "{{" }}.Image{{ "}}" }}\t{{ "{{" }}.Names{{ "}}" }}\t{{ "{{" }}.Status{{ "}}" }}"'
+alias dpu=docker-pull-fn
+alias drun=docker-run-fn
+alias dsdf='docker system df -v'
+alias dsp='docker system prune'
+alias dspa='docker system prune --all'
+alias dsr=docker-stop-rm-fn
+alias dvrm=docker-volume-rm-fn
+alias dvrmd=docker-volume-rm-dangling-fn
+
+alias dirm=docker-image-rm-fn
+alias dnls='docker network ls'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias dvls='docker volume ls'
+
+#==============================================================================#
+#               ------- Docker Compose ------                                  #
+#==============================================================================#
+alias catco='cat compose.yaml'
+alias vico='vim compose.yaml'
+
+# CLI
+alias dc=docker-compose-fn
+alias dcd='docker compose down -v'
+alias dcr='docker compose restart'
+alias dcru=docker-compose-run-fn
+alias dcsta='docker compose start'
+alias dcsto='docker compose stop'
+alias dcu='docker compose up -d'
+alias dcub='docker compose up -d --build'
+
+#==============================================================================#
+#               ------- Terraform ------                                       #
+#==============================================================================#
+alias tf=terraform-fn
+alias tfa='terraform apply'
+alias tfaa='terraform apply --auto-approve'
+alias tfd='terraform destroy'
+alias tfi='terraform init'
+alias tfo='terraform output'
+alias tfp='terraform plan'
+alias tfs=terraform-state-fn
+alias tfsh='terraform show'
+alias tfv='terraform validate'
+
+#==============================================================================#
+#               ------- Bash --------                                          #
+#==============================================================================#
+alias vib='vi ~/.bashrc'
+alias srb='source ~/.bashrc'
+alias cab='cat ~/.bashrc'
---
+++ dotfiles_docker/.config/functions.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+#==============================================================================#
+#               ------- General ------                                         #
+#==============================================================================#
+
+# Add extension $1 to all files without any extension in the current directory
+function add-ext { find . -type f -not -name "*.*" -exec mv "{}" "{}"."$1" \;; }
+
+# Print only column x of output
+function col { awk -v col="$1" "{print $col}"; }
+
+# Create $2 copies of file $1
+function cp-n { EXT="${1##*.}"; FILENAME="${1%.*}"; for i in $(seq 1 "$2"); do cp "$1" "${FILENAME}${i}.${EXT}"; done; }
+
+# Base64 decoding
+function dec { echo "$1" | base64 --decode; }
+
+# Execute $@ command in all the subdirectories
+function exec-sub { find . -maxdepth 1 -mindepth 1 -type d -execdir echo {} \; -execdir $@ {} \; -execdir echo \;; }
+
+# List files with a certain depth
+function lsdp { find . -maxdepth "$1" -ls -path "./.git*" -prune; }
+
+# Make directory $1 and then cd inside
+function mkcd { mkdir "$1"; cd "$1" || return; }
+
+# Tail with continuous monitoring of a given file with syntax highlighting
+function tailc { tail -f "$1" | batcat -l log --paging=never; }
+
+# Host Info
+
+# IP adresses
+function my-ip (){
+  MY_IP=$(/sbin/ifconfig enp8s0 | awk '/inet/ { print $2 }' | sed -e s/addr://)
+}
+
+# Full summary
+function ii (){
+  echo -e "\nYou are logged on ${RED}$(hostname)"
+  echo -e "\nAdditionnal information:$NC" ; uname -a
+  echo -e "\n${RED}Users logged on:$NC" ; w -h
+  echo -e "\n${RED}Current date:$NC" ; date
+  echo -e "\n${RED}Machine stats:$NC" ; uptime
+  echo -e "\n${RED}Memory stats:$NC" ; free
+  my-ip 2>&- ;
+  echo -e "\n${RED}Local IP Address:$NC" ; echo "${MY_IP:-"Not connected"}"
+  echo
+}
+
+# Online cheatsheet
+function cheatsheet { curl cheat.sh/"$1"; }
+
+#==============================================================================#
+#               ------- Server - Environment ------                            #
+#==============================================================================#
+function set-env-fn { export $(grep -v '^#' "${SERVER_HOME}/env/.env" | xargs -d '\n'); }
+function unset-env-fn { unset $(grep -v '^#' "${SERVER_HOME}/env/.env" | sed -E 's/(.*)=.*/\1/' | xargs); }
+
+#==============================================================================#
+#               ------- Bitwarden CLI ------                                   #
+#==============================================================================#
+function bitwarden-create-session-fn {
+  # Prompt for the Bitwarden password securely
+  echo -n 'Enter your Bitwarden password: '
+  read -s BW_PASSWORD
+  echo
+  export BW_PASSWORD
+  # Run the unlock command and set BW_SESSION
+  BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
+  # Check if the command succeeded
+  if [ $? -eq 0 ]; then
+    export BW_SESSION
+    echo 'BW_SESSION set successfully.'
+  else
+    echo 'Failed to set BW_SESSION. Please check your password or Bitwarden setup.'
+  fi
+  # Unset the password variable for security
+  unset BW_PASSWORD
+}
+
+function bitwarden-login-fn {
+  # Prompt for the Bitwarden client id and secret securely
+  echo -n 'Enter your Bitwarden client ID: '
+  read -s BW_CLIENTID
+  echo
+  echo -n 'Enter your Bitwarden client secret: '
+  read -s BW_CLIENTSECRET
+  echo
+  export BW_CLIENTID
+  export BW_CLIENTSECRET
+  # Run the login command
+  bw login --apikey
+  # Unset the API credentials variables for security
+  unset BW_CLIENTID
+  unset BW_CLIENTSECRET
+}
+
+function bitwarden-open-fn {
+  bitwarden-login-fn
+  bitwarden-create-session-fn
+}
+
+#==============================================================================#
+#               ------- Docker ------                                          #
+#==============================================================================#
+function docker-exec-fn { docker exec -it "$1" "${2:-bash}"; }
+function docker-fn { docker "$@"; }
+function docker-image-rm-dangling-fn {
+    IMGS=$(docker images --filter 'dangling=true' -q --no-trunc)
+    [[ -n ${IMGS} ]] && docker rmi ${IMGS} || echo 'no dangling images.'
+}
+function docker-inspect-fn { docker inspect "$1"; }
+function docker-ip-fn {
+  echo 'IP addresses of all named running containers'
+  for DOC in $(docker-names-fn)
+  do
+    IP=$(docker inspect --format='{{ "{{" }}range .NetworkSettings.Networks{{ "}}" }}{{ "{{" }}.IPAddress{{ "}}" }} {{ "{{" }}end{{ "}}" }}' "$DOC")
+    OUT+=$DOC'\t'$IP'\n'
+  done
+  echo -e "$OUT" | column -t
+  unset OUT
+}
+function docker-logs-fn { docker logs -f "$1"; }
+function docker-names-fn {
+	for ID in $(docker ps | awk "{print $1}" | grep -v 'CONTAINER')
+	do
+    docker inspect "$ID" | grep Name | head -1 | awk "{print $2}" | sed 's/,//g' | sed 's%/%%g' | sed 's/"//g'
+	done
+}
+function docker-network-rm-fn { docker network rm "$1"; }
+function docker-pull-fn { docker pull "$1"; }
+function docker-rm-exited-fn { docker rm "$(docker ps --all -q -f status=exited)"; }
+function docker-run-fn { docker run -it "$1" "$2"; }
+function docker-stop-rm-fn { docker stop "$1"; docker rm "$1"; }
+function docker-volume-rm-fn { docker volume rm "$1"; }
+function docker-volume-rm-dangling-fn {
+    VOLS=$(docker volume ls --filter 'dangling=true' -q)
+    [[ -n ${VOLS} ]] && docker volume rm ${VOLS} || echo 'no dangling volumes.'
+}
+
+function docker-image-rm-fn { docker image rm "$@"; }
+
+#==============================================================================#
+#               ------- Docker Compose ------                                  #
+#==============================================================================#
+function docker-compose-fn { docker compose "$@"; }
+function docker-compose-run-fn { docker compose run "$@"; }
+
+#==============================================================================#
+#               ------- Terraform ------                                       #
+#==============================================================================#
+function terraform-fn { terraform "$@"; }
+function terraform-state-fn { terraform state "$@"; }
